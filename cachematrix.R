da9a272a3df7ed26a6060076a05887fdb4b58111
Ann <annabraham82@gmail.com>
2014-08-22 22:53:32 +0530

    Assignment 2 - Caching Inverse of Matrix
    Caching the inverse of a matrix - Calculating the inverse of a square
    matrix using the solve function.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeCacheMatrix creates the matrix that can cache its inverse
+makeCacheMatrix <-function(x=matrix)
+{
+        ## Setting the matrix to NULL
+        matx<-NULL           
+        
+        ## Setting the value
+        setmatx<-function(y)
+        {
+                x<<-y        
+                matx<<-NULL                       
+        }
+        
+        ## Getting the matrix
+        getmatx<-function()x
+        
+        ## Settng the inverse of the matrix
+        setinvmatx<-function(inv) matx <<-inv    
+        
+        ## Getting the inverse of the matrix
+        getinvmatx<-function() matx              
+        
+        list(setmatx=setmatx,getmatx=getmatx,
+             setinvmatx=setinvmatx,getinvmatx=getinvmatx)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## cacheSolve performs the inverse of the matrix
+cacheSolve <-function(x,...)
+{
+        ## Gets the inverse of the matrix
+        matx<-x$getinvmatx()   
+        
+        ## Checks if the inverse were calculated anytime.
+        if(!is.null(matx)){                     
+                
+                message("Get the cached data.")
+                return(matx)
+        }
+        
+        ## Gets the matrix and sets it the data variable
+        data<-x$getmatx()   
+        
+        ## The inverse of the matrix happens here
+        matx<-solve(data,...)    
+        
+        ## Sets the inverse matrix value
+        x$setinvmatx(matx) 
+        
+        # Final result
+        matx
 }
